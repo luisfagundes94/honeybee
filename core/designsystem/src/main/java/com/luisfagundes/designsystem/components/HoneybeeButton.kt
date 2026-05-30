5afeed022044dc5f5460e44ca0ed246cac24aac6
lf <luisfelipecf94@gmail.com>
2026-05-30 14:57:43 -0300

    Remove unused imports from HoneybeeButton

--- core/designsystem/src/main/java/com/luisfagundes/designsystem/components/HoneybeeButton.kt
+++ core/designsystem/src/main/java/com/luisfagundes/designsystem/components/HoneybeeButton.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -15,7 +14,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun HoneybeeButton(
