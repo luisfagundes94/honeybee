f76a4a6aa1fbed1c6e717721766e94df1e88eaff
lf <luisfelipecf94@gmail.com>
2026-05-30 11:57:49 -0300

    Add ResourceProvider DI binding and implement interface in core:common module

--- core/common/src/main/java/com/luisfagundes/core/common/presentation/tools/ResourceProviderImpl.kt
+++ core/common/src/main/java/com/luisfagundes/core/common/presentation/tools/ResourceProviderImpl.kt
@@ -8,8 +8,8 @@ import javax.inject.Singleton
 @Singleton
 internal class ResourceProviderImpl @Inject constructor(
     @param:ApplicationContext private val context: Context
-) {
-    fun getString(resId: Int): String {
+) : ResourceProvider {
+    override fun getString(resId: Int): String {
         return context.getString(resId)
     }
 }
