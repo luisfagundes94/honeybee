44ef90310563ae4845a2a8d9b6a31adec50a227d
lf <luisfelipecf94@gmail.com>
2026-05-30 21:24:56 -0300

    Add consumeWindowInsets to Scaffold content in MainActivity

--- app/src/main/java/com/luisfagundes/honeybee/presentation/MainActivity.kt
+++ app/src/main/java/com/luisfagundes/honeybee/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.CompositionLocalProvider
@@ -49,7 +50,9 @@ class MainActivity : ComponentActivity() {
                                 entryProvider = entryProvider {
                                     entryBuilders.forEach { it(this) }
                                 },
-                                modifier = Modifier.padding(innerPadding)
+                                modifier = Modifier
+                                    .padding(innerPadding)
+                                    .consumeWindowInsets(innerPadding)
                             )
                         }
                     }
