651bc1f4b59f48be28ea15acf6a78ce1fd00406f
lf <luisfelipecf94@gmail.com>
2026-06-12 18:04:24 -0300

    refactor(feedback): remove unused ResourceProvider dependency from FeedbackViewModel

--- feature/config/impl/src/main/java/com/luisfagundes/config/impl/presentation/viewmodel/FeedbackViewModel.kt
+++ feature/config/impl/src/main/java/com/luisfagundes/config/impl/presentation/viewmodel/FeedbackViewModel.kt
@@ -1,21 +1,15 @@
 package com.luisfagundes.config.impl.presentation.viewmodel
 
-import com.luisfagundes.config.impl.R
 import com.luisfagundes.config.impl.presentation.effect.FeedbackUiEffect
 import com.luisfagundes.config.impl.presentation.event.FeedbackUiEvent
 import com.luisfagundes.config.impl.presentation.state.FeedbackUiState
 import com.luisfagundes.core.common.presentation.arch.viewmodel.ViewModel
-import com.luisfagundes.core.common.presentation.tools.ResourceProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-internal class FeedbackViewModel @Inject constructor(
-    private val resourceProvider: ResourceProvider
-) : ViewModel<FeedbackUiState, FeedbackUiEvent, FeedbackUiEffect>(
-    FeedbackUiState()
-) {
-
+internal class FeedbackViewModel @Inject constructor() :
+    ViewModel<FeedbackUiState, FeedbackUiEvent, FeedbackUiEffect>(FeedbackUiState()) {
     override fun dispatchEvent(event: FeedbackUiEvent) {
         when (event) {
             is FeedbackUiEvent.UpdateFeedbackText -> updateFeedbackText(event.text)
