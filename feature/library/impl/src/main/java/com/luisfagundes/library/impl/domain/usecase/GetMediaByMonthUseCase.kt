995298f4e05460de75ce74cf0dba9c05939bf161
lf <luisfelipecf94@gmail.com>
2026-07-18 12:11:14 -0300

    refactor(library): inject and use DefaultDispatcher in GetMediaByMonthUseCase

--- feature/library/impl/src/main/java/com/luisfagundes/library/impl/domain/usecase/GetMediaByMonthUseCase.kt
+++ feature/library/impl/src/main/java/com/luisfagundes/library/impl/domain/usecase/GetMediaByMonthUseCase.kt
@@ -1,17 +1,21 @@
 package com.luisfagundes.library.impl.domain.usecase
 
+import com.luisfagundes.core.common.di.DefaultDispatcher
 import com.luisfagundes.library.api.domain.model.MediaSection
 import com.luisfagundes.library.api.domain.repository.LibraryRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import java.time.Instant
 import java.time.YearMonth
 import java.time.ZoneId
 import javax.inject.Inject
 
 internal class GetMediaByMonthUseCase @Inject constructor(
-    private val repository: LibraryRepository
+    private val repository: LibraryRepository,
+    @param:DefaultDispatcher private val dispatcher: CoroutineDispatcher
 ) {
-    suspend operator fun invoke(): Result<List<MediaSection>> {
-        return repository.getActiveMedia().map { activeMedia ->
+    suspend operator fun invoke(): Result<List<MediaSection>> = withContext(dispatcher) {
+        return@withContext repository.getActiveMedia().map { activeMedia ->
             val mediaByMonth = activeMedia.groupBy { media ->
                 val instant = Instant.ofEpochSecond(media.dateAdded)
                 YearMonth.from(instant.atZone(ZoneId.systemDefault()))
