a340915c0e8ac16594227854ab76354314dbb2a7
lf <luisfelipecf94@gmail.com>
2026-07-24 22:23:00 -0300

    chore(config): bump version to 0.9.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.luisfagundes.honeybee"
         minSdk = 26
         targetSdk = 37
-        versionCode = 8
-        versionName = "0.9.5"
+        versionCode = 9
+        versionName = "0.9.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
